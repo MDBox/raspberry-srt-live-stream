ea9d5ee2b6bdd9e6b13fe95484aa003f3ea160d3
Michael Bawiec <michael.bawiec@gmail.com>
2022-09-11 22:45:09 +0900

    fix threads

--- src/oak_camera_stream.py
+++ src/oak_camera_stream.py
@@ -49,17 +49,17 @@ def stream(
         gst_pipe = GSTProcess(pipe_name, local_port=local_port)
         srt_pipe = SRTProcess(remote_server=remote_server, passphrase=passphrase, local_port=local_port)
 
-        thread_camera = threading.Thread(target=camera_pipe.start())
+        thread_camera = threading.Thread(target=camera_pipe.start)
         thread_camera.daemon = False
         thread_camera.start()
         print('started camera thread')
 
-        thread_gst = threading.Thread(target=gst_pipe.start())
+        thread_gst = threading.Thread(target=gst_pipe.start)
         thread_gst.daemon = False
         thread_gst.start()
         print('started gst thread')
 
-        thread_srt = threading.Thread(target=srt_pipe.start())
+        thread_srt = threading.Thread(target=srt_pipe.start)
         thread_srt.daemon = False
         thread_srt.start()
         print('started srt thread')
